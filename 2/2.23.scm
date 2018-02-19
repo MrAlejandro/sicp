3f2fbc3bf8f682ec86bd94474f9004c235577912
MisterAlejandro <mralejandro777@gmail.com>
2018-02-19 20:46:25 +0400

    Create 2.23.scm

---
+++ 2/2.23.scm
@@ -0,0 +1,13 @@
+(define (for-each proc items)
+    (if (null? items)
+        #t
+        (let
+            ((dummy (proc (car items))))
+            (for-each proc (cdr items))
+        )
+    )
+)
+
+(for-each (lambda (x) (newline) (display x))
+          (list 57 321 88)
+)
