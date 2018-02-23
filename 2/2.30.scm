b899430b900086bd831543ebe7c790539dfa7fe5
MisterAlejandro <mralejandro777@gmail.com>
2018-02-23 12:21:33 +0400

    Update 2.30.scm

--- 2/2.30.scm
+++ 2/2.30.scm
@@ -2,6 +2,21 @@
 (display test-list)
 (newline)
 
+(define (square x) (* x x))
+(define (square-tree lst)
+    (define (iter result lst)
+        (cond
+            ((null? lst) result)
+            ((pair? (car lst)) (iter (append result (list (iter '() (car lst)))) (cdr lst)))
+            (else (iter (append result (list (square (car lst)))) (cdr lst)))
+        )
+    )
+    (iter '() lst)
+)
+
+(display (square-tree test-list))
+(newline)
+
 ; using map
 (define (square-tree lst)
     (map
