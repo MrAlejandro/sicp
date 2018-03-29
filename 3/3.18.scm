69aef873699c757597c2c65163e6a9d4e71bc853
MisterAlejandro <mralejandro777@gmail.com>
2018-03-29 19:54:50 +0400

    Update 3.18.scm

--- 3/3.18.scm
+++ 3/3.18.scm
@@ -10,22 +10,46 @@
     )
 )
 
-(define (count-pairs x)
-    (define encountered '())
-    (define (iter x)
-        (if (or (not (pair? x)) (memq x encountered))
-            0
-            (begin
-                (set! encountered (cons x encountered))
-                (+
-                    (iter (car x))
-                    (iter (cdr x))
-                    1
+; checks if list has recursive links
+(define (is-recursive x)
+    (define (iter x parents)
+        (if (not (pair? x))
+            #f
+            (if (memq x parents)
+                #t
+                (or
+                    #f
+                    (iter (car x) (cons x parents))
+                    (iter (cdr x) (cons x parents))
                 )
             )
         )
     )
-    (iter x)
+    (iter x '())
+)
+
+(define (count-pairs x)
+    (if (is-recursive x)
+        "Recursive links"
+        (let
+            ((encountered '()))
+            (define (iter x)
+                (if (or (not (pair? x)) (memq x encountered))
+                    0
+                    (begin
+                        (set! encountered (cons x encountered))
+                        (+
+                            (iter (car x))
+                            (iter (cdr x))
+                            1
+                        )
+                    )
+                )
+            )
+            (iter x)
+        )
+        
+    )
 )
 
 (define three '(a b c))
@@ -41,4 +65,13 @@
 (define second (cons common-pair common-pair))
 (define seven (cons second second))
 (display (count-pairs seven))
+(newline)
+
+(define third-pair '(e f))
+(define second-pair (cons 'c third-pair))
+(define first-pair (cons 'a second-pair))
+(set-cdr! third-pair first-pair)
+
+(define recursive (cons first-pair (cons second-pair third-pair)))
+(display (count-pairs recursive))
 (newline)
